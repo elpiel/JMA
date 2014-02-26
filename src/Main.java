f00c2940350ea1a3bb24adb770b528d895d5e5c7
raptor793 <shadow793@abv.bg>
2014-02-26 16:08:41 +0200

    Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("OK");
-
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.JTabbedPane;
+public class Main extends JFrame{
+	private JTabbedPane tab;
+	
+	public Main(){
+		super("JMA");
+		components();
 	}
-
+	
+	public void components(){
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+		tab = new JTabbedPane();
+		tab.setName("Test");
+		add(tab);
+	}
+	
+	public static void main(String[] args){
+		Main window = new Main();
+		window.setSize(600, 500);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+	}
 }
